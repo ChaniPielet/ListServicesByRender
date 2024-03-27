2bde5a11103dddc87c38aa22444bfe5959707802
chani <c0556791657@gmail.com>
2024-03-27 16:15:36 +0200

    key

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ app.get('', (req, res) => {
     res.status(200).send(good)
 })
 
-const sdk = require('api')('@render-api/v1.0#ja8rb1jlsxhh7qu');
+// const sdk = require('api')('@render-api/v1.0#ja8rb1jlsxhh7qu');
 
-sdk.auth('rnd_0H3CaJvsXwn4NnKLctZEb1X8SzJB');
-sdk.getServices({limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+// sdk.auth('rnd_0H3CaJvsXwn4NnKLctZEb1X8SzJB');
+// sdk.getServices({limit: '20'})
+//   .then(({ data }) => console.log(data))
+//   .catch(err => console.error(err));
 app.listen(3001, () => {
     console.log(`my app is listening in http://localhost:3001`);
 })
